e3edbc49391c8162df1f2b0935075bbaa07fa316
fareed gul <fareedgul735@gmail.com>
2025-06-02 00:58:25 +0500

    add my ticket-management signup page changing

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,141 +1,144 @@
-import { Form, Input, Button, FloatButton } from "antd";
+import { Button, Col, Form, Input, Row } from "antd";
 import "./Signup.css";
-import { PAKISTAN_CNIC_PATTERN, PASSWORD_PATTERN } from "../../lib/regex";
+import { PASSWORD_PATTERN } from "../../lib/regex";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router";
 
 const Signup = () => {
-  const navigate = useNavigate();
+  const nevigate = useNavigate();
 
   const onFinish = async (data) => {
-    console.log(data, "===data");
-    const result = await Swal.fire({
-      title: "error",
-      text: "You have signedup successfully",
-      icon: "error",
+    console.log(data);
+    const response = await Swal.fire({
+      title: "Confirmation",
+      text: "Are you sure you want to sign up?",
+      showCancelButton: true,
+      confirmButtonText: "Sure",
+      cancelButtonText: "Cancel",
+      background: "#fff",
+      color: "#000",
+      customClass: {
+        popup: "my-custom-modal",
+        confirmButton: "my-confirm-btn",
+        cancelButton: "my-cancel-btn",
+      },
+      position: "center",
+      width: "370px",
     });
-    if (result.isConfirmed) {
-      navigate("/dashboard");
+    if (response.isConfirmed) {
+      nevigate("/dashboard");
     }
-    console.log(result, "===result");
   };
-  return (
-    <div className="signup-container">
-      <Form
-        className="signup-form"
-        onFinish={onFinish}
-        // colon={false}
-        // initialValues={initialValues}
-        // disabled={true}
-        // size="large"
-      >
-        <Form.Item
-          //   colon={false}
-          //   extra={<p>Pakistan</p>}
-          //   help={<p>Pakistan</p>}
-          label="Full Name"
-          name={"fullName"}
-          rules={[
-            {
-              required: true,
-              message: "Full name is required",
-              whitespace: true,
-            },
-            // {
-            //   min: 3,
-            //   message: "Full name must be greater than 3",
-            //   //   warningOnly: true,
-            // },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Username"
-          name={"username"}
-          rules={[
-            {
-              required: true,
-              message: "Email is required",
-              whitespace: true,
-            },
-            {
-              min: 3,
-              message: "Must be greater than or equal to 3 char",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Email"
-          name={"email"}
-          rules={[
-            {
-              required: true,
-              message: "Email is required",
-              whitespace: true,
-            },
-            {
-              type: "email",
-              message: "Enter a valid email",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Phone No."
-          name={"phone"}
-          rules={[
-            {
-              min: 11,
-              message: "Enter valid mobile",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="CNIC"
-          name={"cnic"}
-          rules={[
-            {
-              required: true,
-              message: "CNIC is required",
-            },
-            {
-              pattern: PAKISTAN_CNIC_PATTERN,
-              message: "Please enter valid cnic",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name={"password"}
-          type={"password"}
-          rules={[
-            {
-              required: true,
-              message: "Password is required",
-            },
-            {
-              pattern: PASSWORD_PATTERN,
-              message:
-                "Password must be 8+ chars, include uppercase, lowercase, number & special char.",
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
+  return (
+    <div className="wrapper">
+      <Form className="form-container" onFinish={onFinish}>
+        <Row gutter={6}>
+          <Col sm={24} md={12}>
+            <Form.Item
+              label="FullName"
+              name={"fullName"}
+              colon={false}
+              rules={[
+                {
+                  required: true,
+                  whitespace: true,
+                },
+                {
+                  min: 3,
+                },
+              ]}
+            >
+              <Input placeholder="Enter the FullName" />
+            </Form.Item>
+          </Col>
+          <Col sm={24} md={12}>
+            <Form.Item
+              label="UserName"
+              name={"userName"}
+              colon={false}
+              rules={[
+                {
+                  required: true,
+                  whitespace: true,
+                },
+                {
+                  min: 3,
+                },
+              ]}
+            >
+              <Input placeholder="Enter the UserName" />
+            </Form.Item>
+          </Col>
+          <Col sm={24} md={12}>
+            <Form.Item
+              label="Email"
+              name={"email"}
+              colon={false}
+              rules={[
+                {
+                  required: true,
+                  whitespace: true,
+                },
+              ]}
+            >
+              <Input placeholder="Enter the Email" />
+            </Form.Item>
+          </Col>
+          <Col sm={24} md={12}>
+            <Form.Item
+              label="Password"
+              name={"password"}
+              colon={false}
+              rules={[
+                {
+                  required: true,
+                  whitespace: true,
+                },
+                {
+                  pattern: PASSWORD_PATTERN,
+                  message:
+                    "Password must be 8+ chars, include uppercase, lowercase, number & special char.",
+                },
+              ]}
+            >
+              <Input.Password placeholder="Enter the Password" />
+            </Form.Item>
+          </Col>
+          <Col sm={24} md={12}>
+            <Form.Item
+              label="CNIC"
+              name={"cnic"}
+              type={"number"}
+              colon={false}
+              rules={[
+                {
+                  required: true,
+                  whitespace: true,
+                },
+              ]}
+            >
+              <Input placeholder="Enter the CNIC Number" />
+            </Form.Item>
+          </Col>
+          <Col sm={24} md={12}>
+            <Form.Item
+              label="PhoneNumber"
+              name={"phoneNumber"}
+              type={"number"}
+              colon={false}
+            >
+              <Input placeholder="Enter the Phone Number" />
+            </Form.Item>
+          </Col>
+          <Col sm={24} md={12} offset={12}>
+            <Form.Item style={{ textAlign: "right" }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
       </Form>
     </div>
   );
