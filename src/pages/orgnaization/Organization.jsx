784fc2009d92f01462218f9e92772aa0c73d0ce0
fareed gul <fareedgul735@gmail.com>
2025-06-16 00:42:11 +0500

    add the organization & orgaization form changes & functionalities

--- src/pages/orgnaization/Organization.jsx
+++ src/pages/orgnaization/Organization.jsx
@@ -1,99 +1,91 @@
-// import { Button, Table } from "antd";
-// import { useEffect, useState } from "react";
-// import { Link } from "react-router";
-// import { DB_COLLECTION } from "../../lib/constant";
-// import { collection, db, getDocs, query, where } from "../../lib/firebase";
-// import { DeleteOutlined, EditOutlined, EyeOutlined } from "@ant-design/icons";
+import { Button, Table } from "antd";
+import { DeleteOutlined, EditOutlined, EyeOutlined } from "@ant-design/icons";
+import { Link } from "react-router";
+import { useEffect, useState } from "react";
+import { collection, db, getDocs, query, where } from "../../lib/firebase";
+import { DB_COLLECTION } from "../../lib/constant";
+import Swal from "sweetalert2";
+const Organization = () => {
 
-// const Organization = () => {
-//     // const [currentPage, setCurrentPage] = useState(1);
-//     const [dataSource, setDataSource] = useState([]);
-//     const columns = [
-//         { title: "Name", dataIndex: "name" },
-//         { title: "Contact", dataIndex: "contact" },
-//         {
-//             title: "NTN No.",
-//             dataIndex: "ntn",
-//             render: (value) => value || "-",
-//         },
-//         {
-//             title: "Location",
-//             dataIndex: "location",
-//             render: (value, record) => {
-//                 console.log(value, record);
-//                 return value.replaceAll(" ", ", ");
-//             },
-//         },
-//         {
-//             title: "Actions",
-//             dataIndex: "action",
-//             render: (_, record) => {
-//                 const { id } = record;
-//                 const onDelete = () => {
-//                     alert(id);
-//                 };
-//                 return (
-//                     <div>
-//                         <Button type="text">
-//                             <EditOutlined />
-//                         </Button>
-//                         <Button type="text" onClick={onDelete}>
-//                             <DeleteOutlined />
-//                         </Button>
-//                         <Button type="text">
-//                             <EyeOutlined />
-//                         </Button>
-//                     </div>
-//                 );
-//             },
-//         },
-//     ];
-//     const fetchData = async () => {
-//         const userId = localStorage.getItem("userId");
-//         const parsedData = [];
-//         const docRef = collection(db, DB_COLLECTION.ORGANIZATIONS);
-//         const customQuery = where("userId", "==", userId);
-//         const qRef = query(docRef, customQuery);
-//         const querySnapshot = await getDocs(qRef);
-//         querySnapshot.forEach((doc) => {
-//             const data = { id: doc.id, ...doc.data() };
-//             parsedData.push(data);
-//         });
-//         // console.log(parsedData);
-//         setDataSource(parsedData);
-//     };
+    const [dataSource, setDataSource] = useState([]);
 
-//     useEffect(() => {
-//         fetchData();
-//     }, []);
-//     return (
-//         <div>
-//             <Link to={"form"}>
-//                 <Button
-//                     style={{ margin: "2px", float: "right" }}
-//                     color="blue"
-//                     variant="solid"
-//                 >
-//                     Add Organization
-//                 </Button>
-//             </Link>
-//             <Table
-//                 columns={columns}
-//                 dataSource={dataSource}
-//                 pagination={{
-//                     position: ["bottomCenter"],
-//                     pageSize: 5,
-//                     // current: currentPage,
-//                     // onChange: function () {
-//                     //   alert("page changed");
-//                     //   setCurrentPage((pre) => ++pre);
-//                     // },
-//                 }}
-//             />
-//         </div>
-//     );
-// };
+    const columns = [
+        { title: "Name", dataIndex: "name", },
+        { title: "Contact", dataIndex: "contact" },
+        { title: "Ntn Number", dataIndex: "ntn", render: (value) => value || "-" },
+        { title: "Location", dataIndex: "location" },
+        {
+            title: "Actions", dataIndex: "actions",
+            render: (_, records) => {
+                const onDelete = () => {
+                    const { id } = records;
+                    alert(id)
+                }
+                const onView = () => {
+                    const { id } = records
+                    alert(id)
+                }
+                const onEdit = () => {
+                    const { id } = records
+                    alert(id)
 
-// export default Organization;
+                }
+                return (
+                    <div className="actions-btn">
+                        <Button>
+                            <DeleteOutlined onClick={onDelete} />
+                        </Button>
+                        <Button>
+                            <EditOutlined onClick={onEdit} />
+                        </Button>
+                        <Button>
+                            <EyeOutlined onClick={onView} />
+                        </Button>
+                    </div>
+
+                )
+            }
+
+        }
+    ]
+
+    const fetchingData = async () => {
+        const parsedData = [];
+        try {
+            const userId = localStorage.getItem("userId");
+            const collectionRef = collection(db, DB_COLLECTION.ORGANIZATIONS);
+            const customQuery = where("userId", "==", userId);
+            const qRef = query(collectionRef, customQuery);
+            const querySnapShot = await getDocs(qRef);
+            querySnapShot.forEach((docs) => {
+                const data = { id: docs.id, ...docs.data() }
+                parsedData.push(data)
+            })
+            setDataSource(parsedData)
+        } catch (err) {
+            await Swal.fire({
+                text: 'Internal Server Error',
+            })
+        }
+    }
+
+    useEffect(() => {
+        fetchingData()
+    }, [])
 
 
+    return (
+        <div>
+            <Link to={"form"}>
+                <Button type="primary" style={{ float: "right", margin: "6px ,6px" }}>
+                    Add Organization
+                </Button>
+            </Link>
+
+            <Table columns={columns} dataSource={dataSource} pagination={{ position: ["bottomCenter"], pageSize: 10 }} />
+
+        </div>
+    )
+}
+
+export default Organization
--- src/pages/orgnaization/OrganizationForm.jsx
+++ src/pages/orgnaization/OrganizationForm.jsx
@@ -1,126 +1,82 @@
-// import { Form, Input, Button, Row, Col } from "antd";
-// import { useNavigate } from "react-router";
-// import { addDoc, collection, db } from "../../lib/firebase";
-// import { DB_COLLECTION, } from "../../lib/constant";
-// import Swal from "sweetalert2";
+import { Button, Form, Input } from "antd"
+import { addDoc, collection, db } from "../../lib/firebase"
+import { DB_COLLECTION } from "../../lib/constant"
+import Swal from "sweetalert2"
+import { useNavigate } from "react-router"
 
-// const OrganizationForm = () => {
-//   const navigate = useNavigate();
+const OrganizationForm = () => {
+  const navigate = useNavigate()
 
-//   const saveOrganizationDetails = async (organizationDetails, userId) => {
-//     const organizationDetailsPayload = { userId, ...organizationDetails };
-//     const collectionRef = collection(db, DB_COLLECTION.ORGANIZATIONS);
-//     console.log(collectionRef);
-//     await addDoc(collectionRef, organizationDetailsPayload);
-//   };
+  const saveOrganization = async (userDetails, userId) => {
+    const payload = { userId, ...userDetails }
+    const collectionRef = collection(db, DB_COLLECTION.ORGANIZATIONS);
+    await addDoc(collectionRef, payload)
+  }
 
-//   const getUserConfirmation = async () => {
-//     const result = await Swal.fire({
-//       title: "Confirmation",
-//       text: "Are You Sure, You want to add organization",
-//       showCancelButton: true,
-//       confirmButtonText: "Sure",
-//     });
+  const getUserConfirmation = async () => {
+    const result = Swal.fire({
+      title: "Confirmation",
+      text: "Are You Sure, You want to add organization",
+      showCancelButton: true,
+      confirmButtonText: "Sure",
+    })
+    return (await result).isConfirmed
+  }
 
-//     return result.isConfirmed;
-//   };
+  const onDataSuccessfully = async (data) => {
+    const userId = localStorage.getItem("userId")
+    try {
 
-//   const onFinish = async (data) => {
-//     try {
-//       const isUserConfirmed = await getUserConfirmation();
-//       if (isUserConfirmed) {
-//         const userId = localStorage.getItem("userId");
-//         await saveOrganizationDetails(data, userId);
-//         navigate("/organization");
-//       }
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   };
-//   return (
-//     <div className="wrapper">
-//       <Form
-//         className="login-form"
-//         onFinish={onFinish}
-//         validateMessages={{ required: "Please fill this '${name}'" }}
-//         labelCol={{ span: 4 }}
-//         labelAlign="left"
-//       >
-//         <Row gutter={6}>
-//           <Col sm={24}>
-//             <Form.Item
-//               label="Name"
-//               name={"name"}
-//               rules={[
-//                 {
-//                   required: true,
-//                   whitespace: true,
-//                 },
-//                 {
-//                   min: 2,
-//                 },
-//                 {
-//                   max: 25,
-//                 },
-//               ]}
-//             >
-//               <Input />
-//             </Form.Item>
-//           </Col>
-//           <Col sm={24}>
-//             <Form.Item
-//               label="Location"
-//               name={"location"}
-//               rules={[
-//                 {
-//                   required: true,
-//                 },
-//               ]}
-//             >
-//               <Input />
-//             </Form.Item>
-//           </Col>
-//           <Col sm={24}>
-//             <Form.Item
-//               label="NTN No."
-//               name={"ntn"}
-//               type="number"
-//               rules={[
-//                 {
-//                   required: true,
-//                 },
-//                 // {
-//                 //   type: "number",
-//                 // },
-//               ]}
-//             >
-//               <Input type="number" />
-//             </Form.Item>
-//           </Col>
-//           <Col sm={24}>
-//             <Form.Item
-//               label="Contact No."
-//               name={"contact"}
-//               rules={[
-//                 {
-//                   required: true,
-//                 },
-//               ]}
-//             >
-//               <Input type="number" />
-//             </Form.Item>
-//           </Col>
-//           <Col md={12} sm={24} offset={12}>
-//             <Form.Item style={{ textAlign: "right" }}>
-//               <Button type="primary" htmlType="submit" ali>
-//                 Submit
-//               </Button>
-//             </Form.Item>
-//           </Col>
-//         </Row>
-//       </Form>
-//     </div>
-//   );
-// };
+      const isUserConfirmed = await getUserConfirmation();
+      if (isUserConfirmed) {
+        await saveOrganization(data, userId)
+        navigate("/organization")
+      }
 
-// export default OrganizationForm;
+    } catch (err) {
+      await Swal.fire({
+        title: "Internal Server Error"
+      })
+    }
+  }
+
+
+  return (
+    <div>
+      <div className="wrapper">
+        <Form className="form-container" onFinish={onDataSuccessfully}>
+          <Form.Item label={"Name"} name={"name"} rules={[
+            {
+              required: true,
+            }, {
+              min: 3,
+              message: "please enter the correct name "
+            }
+          ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item label={"Contact"} name={"contact"} rules={[
+            {
+              required: true,
+            },
+          ]}>
+            <Input type="number" />
+          </Form.Item>
+          <Form.Item label={"Ntn No"} name={"ntn"} >
+            <Input type="number" />
+          </Form.Item>
+          <Form.Item label={"Location"} name={"location"} >
+            <Input />
+          </Form.Item>
+          <Form.Item  >
+            <Button htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default OrganizationForm
